74e94b36efad937d24c24bbc2ef075df29ef1615
RatoRising <reinaneves@hotmail.com>
2023-04-04 18:41:40 -0300

    test files being used

--- dist/js/ztests.js
+++ dist/js/ztests.js
@@ -1,29 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// let object: Object = {
-//   id: 1,
-//   name: "rato",
-// };
-// function myFunction(object: Object): void {
-//   typeof object === "object" &&
-//     Object.entries(object).forEach((entry) => console.log(entry));
-// }
-// myFunction(object);
-// function A() {
-//   console.log("called A");
-//   return false;
-// }
-// function B() {
-//   console.log("called B");
-//   return true;
-// }
-// console.log(A() && B());
-let str = "boost the world together!";
-// function reveseStr() {
-//   let arr = Array.from(str)
-//   return arr.reverse().join('')
-// }
-function reveseStr() {
-    return str.split('').reverse().join("");
+let object = {
+    id: 1,
+    name: "rato",
+};
+function myFunction(object) {
+    typeof object === "object" &&
+        Object.entries(object).forEach((entry) => console.log(entry));
 }
-console.log(reveseStr());
+myFunction(object);
+function A() {
+    console.log("called A");
+    return false;
+}
+function B() {
+    console.log("called B");
+    return true;
+}
+console.log(A() && B());
+const reverseStr1 = (str) => console.log(Array.from(str).reverse().join(''));
+reverseStr1("Boost the world together!");
+const reverseStr2 = (str) => console.log(str.split('').reverse().join(''));
+reverseStr2("Boost the world together!");
--- src/ts/ztests.ts
+++ src/ts/ztests.ts
@@ -1,36 +1,29 @@
-export{}
-// let object: Object = {
-//   id: 1,
-//   name: "rato",
-// };
+export {};
+let object: Object = {
+  id: 1,
+  name: "rato",
+};
 
-// function myFunction(object: Object): void {
-//   typeof object === "object" &&
-//     Object.entries(object).forEach((entry) => console.log(entry));
-// }
+function myFunction(object: Object): void {
+  typeof object === "object" &&
+    Object.entries(object).forEach((entry) => console.log(entry));
+}
 
-// myFunction(object);
+myFunction(object);
 
-// function A() {
-//   console.log("called A");
-//   return false;
-// }
-// function B() {
-//   console.log("called B");
-//   return true;
-// }
+function A() {
+  console.log("called A");
+  return false;
+}
+function B() {
+  console.log("called B");
+  return true;
+}
+console.log(A() && B());
 
-// console.log(A() && B());
 
-let str: string = "boost the world together!"
+const reverseStr1 = (str: string) => console.log(Array.from(str).reverse().join(''))
+reverseStr1("Boost the world together!");
 
-// function reveseStr() {
-//   let arr = Array.from(str)
-//   return arr.reverse().join('')
-// }
-
-function reveseStr() {
-    return str.split('').reverse().join("")
-  }
-
-console.log(reveseStr());
+const reverseStr2 = (str: string) => console.log(str.split('').reverse().join(''))
+reverseStr2("Boost the world together!");
