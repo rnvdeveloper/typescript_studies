61a78dc49cfb354f6ece86b6ceeaeef3df8a4d94
RatoRising <reinaneves@hotmail.com>
2023-04-10 19:13:34 -0300

    updates type_assertion files

--- src/ts/ztests.ts
+++ src/ts/ztests.ts
@@ -1,23 +1,23 @@
-export {};
-let object: Object = {
-  id: 1,
-  name: "rato",
-};
+// export {};
+// let object: Object = {
+//   id: 1,
+//   name: "rato",
+// };
 
-function myFunction(object: Object): void {
-  typeof object === "object" &&
-    Object.entries(object).forEach((entry) => console.log(entry));
-}
+// function myFunction(object: Object): void {
+//   typeof object === "object" &&
+//     Object.entries(object).forEach((entry) => console.log(entry));
+// }
 
-myFunction(object);
+// myFunction(object);
 
-function A() {
-  console.log("called A");
-  return false;
-}
-function B() {
-  console.log("called B");
-  return true;
-}
-console.log(A() && B()); // If A is true it will return A and call B, if A is false it will only return A
+// function A() {
+//   console.log("called A");
+//   return false;
+// }
+// function B() {
+//   console.log("called B");
+//   return true;
+// }
+// console.log(A() && B()); // If A is true it will return A and call B, if A is false it will only return A
 
