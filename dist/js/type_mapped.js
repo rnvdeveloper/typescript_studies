6eb42268abaac049422e1ef3495117b210abbacb
RatoRising <reinaneves@hotmail.com>
2023-04-10 21:50:31 -0300

    create mapped type files

---
+++ dist/js/type_mapped.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const userMapped = {
+    name: 'Rnv Developer',
+    address: 'Development Street, 1'
+};
+console.log(userMapped);
+const book1 = {
+    author: "A Author",
+    title: 'A title',
+    price: 20.00
+};
+const book2 = {
+    author: "A Author",
+    title: 'A title',
+    pages: 200,
+    price: 20.00
+};
+console.log(book1);
+console.log(book2);
