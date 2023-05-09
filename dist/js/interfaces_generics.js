bff044dc398ac1f7b7b2683a1b4b48efaa7eb0f6
RatoRising <reinaneves@hotmail.com>
2023-05-09 20:34:59 -0300

    add modules directories and files

--- dist/js/interfaces_generics.js
+++ dist/js/interfaces_generics.js
@@ -1,11 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const isomorphic_fetch_1 = __importDefault(require("isomorphic-fetch"));
 async function fetchJson(url) {
-    const response = await (0, isomorphic_fetch_1.default)(url);
+    const response = await fetch(url);
     const headers = {};
     response.headers.forEach((value, key) => {
         headers[key] = value;
---
+++ dist/js/modules/module.math.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Divide = exports.Multiply = exports.Subtract = exports.Sum = void 0;
+function Sum(x, y) {
+    return x + y;
+}
+exports.Sum = Sum;
+function Subtract(x, y) {
+    return x - y;
+}
+exports.Subtract = Subtract;
+function Multiply(x, y) {
+    return x * y;
+}
+exports.Multiply = Multiply;
+function Divide(x, y) {
+    return x / y;
+}
+exports.Divide = Divide;
