2d1cc8100d0c7632a81a7cbb489a905718975243
RatoRising <reinaneves@hotmail.com>
2023-05-18 23:50:58 -0300

    add final project with typescript and express

---
+++ project/ts_node_express/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 10;
+app.use((0, helmet_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+    res.send(`<h1>Welcome to Glaucia Lemos' Zero to Hero Typescript course!</h1>`);
+});
+app.listen(port, () => console.log(`Server running on port ${port}`));
